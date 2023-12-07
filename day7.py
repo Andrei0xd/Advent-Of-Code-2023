cd150539bd727808351e02563f9cc25521ef0582
Andrei0xD <andrei.cirnu13@gmail.com>
2023-12-07 12:34:08 +0200

    more tidy day 7

--- day7.py
+++ day7.py
@@ -1,28 +1,21 @@
-import re
+import re, functools
 
 with open("./inputs/day7.in", "r") as file:
     lines = file.readlines()
 
 xtra = {"J" :0, "T" :10, "Q" :11, "K" :12, "A" :13}
 
-class hand():
-    def __init__(self, hashedpairs, bid, hstr):
-        self.hashedpairs = hashedpairs
-        self.bid = bid
-        self.hstr = hstr
-
-    def __lt__(self, other):
-        if self.hashedpairs == other.hashedpairs:
-            for i in range(len(self.hstr)):
-                if not self.hstr[i] == other.hstr[i]:
-                    return (int(self.hstr[i]) if self.hstr[i] not in xtra else xtra[self.hstr[i]]) < (int(other.hstr[i]) if other.hstr[i] not in xtra else xtra[other.hstr[i]])
-        else:
-            return self.hashedpairs < other.hashedpairs
-
+def comparator(self, other):
+    if self[0] == other[0]:
+        for i in range(len(self[1])):
+            if not self[1][i] == other[1][i]:
+                return (int(self[1][i]) if self[1][i] not in xtra else xtra[self[1][i]]) - (int(other[1][i]) if other[1][i] not in xtra else xtra[other[1][i]])
+    else:
+        return self[0] - other[0]
+        
 hands = []
 for line in lines:
     handstr = line.strip().split(" ")[0]
-
     handstrsort = ''.join(sorted(handstr))
     pairs = re.finditer(r"(.)\1*", handstrsort)
     
@@ -35,11 +28,9 @@ for line in lines:
 
     pairs_hash[pairs_hash.index(max(pairs_hash))] = max(pairs_hash)+jokers
 
-    hashedpairs=0
-    for s in pairs_hash:
-        hashedpairs += 10**s
-        
-    hands.append(hand(hashedpairs, line.strip().split(" ")[1], handstr))
+    hashedpairs=sum([10**s for s in pairs_hash])
+    
+    hands.append((hashedpairs,  handstr, line.strip().split(" ")[1]))
 
-sum = sum([int(hand.bid)*(r+1) for r,hand in enumerate(sorted(hands))])
+sum = sum([int(hand[2])*(r+1) for r,hand in enumerate(sorted(hands, key=functools.cmp_to_key(comparator)))])
 print(sum)
